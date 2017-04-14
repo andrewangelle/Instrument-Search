cdc11392cbfa580450cdd337d45e68fff5f88909
andrewangelle <andrewangelle@gmail.com>
2017-04-14 11:35:05 -0500

    example folder

---
+++ example/example.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import InstrumentSearch from './src/indexs';
+
+ReactDOM.render(
+  <InstrumentSearch />,
+  document.getElementById('root')
+);
