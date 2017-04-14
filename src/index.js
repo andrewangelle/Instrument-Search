34b0000659d1192c4ed0166df12e44bd68ce7315
andrewangelle <andrewangelle@gmail.com>
2017-04-13 23:27:16 -0500

    cleaned up src folder

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import InstrumentSearch from './InstrumentSearch';
+
 
 ReactDOM.render(
-  <App />,
+  <InstrumentSearch />,
   document.getElementById('root')
 );
