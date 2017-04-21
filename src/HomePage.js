e751d600d9d685890e1ad69c513fe2bbb17b706f
andrewangelle <andrewangelle@gmail.com>
2017-04-21 08:18:46 -0500

    added nav links with router

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,10 +1,37 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Switch 
+} from 'react-router-dom'
+import createHistory from 'history/createBrowserHistory';
 import InstrumentSearch from './InstrumentSearch';
+import AdminHome from './AdminHome';
+
+
+const history = createHistory();
 
 export default class HomePage extends React.Component {
-	render(){
-		return(
-			<InstrumentSearch />
-		);
-	}
+  render(){
+	return(
+	  <Router history={history} >
+	    <div>
+		  <ul>
+		    <li><Link to="/home">Home</Link></li>
+		    <li><Link to="/search">Search</Link></li>
+		    <li><Link to="/admin">Admin</Link></li>
+		  </ul>
+
+		  <hr/>
+
+          <Switch>
+            <Route exact path="/home" component={HomePage} />
+            <Route exact path="/search" component={InstrumentSearch} />
+            <Route exact path="/admin" component={AdminHome} />
+          </Switch> 	
+		</div>     
+      </Router> 	
+	);
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,23 +1,10 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Router, Route, Switch } from 'react-router-dom';
-import createHistory from 'history/createBrowserHistory';
 import HomePage from './HomePage';
-import NextPage from './NextPage'
-import AdminHome from './AdminHome';
-
-const history = createHistory();
 
 ReactDOM.render(
-      <Router history={history} >
-        <Switch>
-          <Route exact path="/" component={HomePage} />
-          <Route exact path="/next" component={NextPage} />
-          <Route exact path="/admin" component={AdminHome} />
-        </Switch>      
-      </Router>, 
-
-	document.getElementById('root')
+  <HomePage />,
+  document.getElementById('root')
 );  
 
