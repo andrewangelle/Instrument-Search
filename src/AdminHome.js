df39a89d717eec51e585173f41ba66221d7befec
andrewangelle <andrewangelle@gmail.com>
2017-04-21 03:51:32 -0500

    just kidding. Now theyre uploaded

---
+++ src/AdminHome.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+export default class AdminHome extends React.Component {
+
+	constructor(props){
+		super(props);
+
+		this.state={
+			instruments: [ ]
+		};
+
+		this.updateResults = this.updateResults.bind(this);
+	}
+
+  	updateResults(results) {
+      this.setState({instruments: results});
+  	}
+
+	componentDidMount() {
+
+      fetch('api/search?')
+       .then(function(response) { 
+         return response.json();
+       })
+       .then(results => {
+         this.updateResults(results);
+         console.log(results);
+    	});
+	}
+	
+
+	render(){
+		return(
+			<div>
+      			<ul>
+        		  {this.state.instruments.map(instrument =>
+          		    <li key={instrument.id}> 
+              		  <button>{instrument.name}</button>
+          		    </li>        
+        		  )}
+      			</ul>
+			</div>
+		);
+	}
+}
+
