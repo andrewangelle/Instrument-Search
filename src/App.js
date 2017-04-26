d5c19f4cdcc74f9075116ea1dfde27f25ff03d4b
andrewangelle <andrewangelle@gmail.com>
2017-04-25 20:26:11 -0500

    added <Route/> for /admin/create. Attached placeholder component

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import createHistory from 'history/createBrowserHistory';
 import InstrumentSearch from './InstrumentSearch';
 import AdminHome from './AdminHome';
 import AdminEdit from './AdminEdit';
+import AdminCreate from './AdminCreate';
 
 
 const history = createHistory();
@@ -20,7 +21,8 @@ export default class App extends React.Component {
           <Switch>
             <Route exact path="/" component={InstrumentSearch} />
             <Route exact path="/admin" component={AdminHome} />
-            <Route exact path="/admin/edit/:id" component={AdminEdit} />           
+            <Route exact path="/admin/edit/:id" component={AdminEdit} />
+            <Route exact path="/admin/create/" component={AdminCreate} />           
           </Switch> 
         </div>     
       </Router>
